4b58550a8862aa6918738e59ced3f014c0e740c9
DevD1k <113674752+DevD1k@users.noreply.github.com>
2023-04-24 19:12:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 function highlight() {
-    //Write your code here
+  const boldWords = document.querySelectorAll("strong");
+  for (let i = 0; i < boldWords.length; i++) {
+    boldWords[i].style.color = "green";
+  }
+}
 
-
+function return_normal() {
+  const boldWords = document.querySelectorAll("strong");
+  for (let i = 0; i < boldWords.length; i++) {
+    boldWords[i].style.color = "black";
+  }
 }
 
 
