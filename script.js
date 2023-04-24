11206d6fe6cbb65b605b78b1c95357784f5e0278
DevD1k <113674752+DevD1k@users.noreply.github.com>
2023-04-24 19:20:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function highlight() {
   const boldWords = document.querySelectorAll("strong");
   for (let i = 0; i < boldWords.length; i++) {
-    boldWords[i].style.color = "green";
+    boldWords[i].style.color = "rgb(0,128,0)";
   }
 }
 
